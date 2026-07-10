b36b7b9e3c995d1ef39f8b9093692f4b77f00d66
Deva92dev <buildwith92deva@gmail.com>
2026-07-10 13:02:59 +0530

    first commit

---
+++ Phase 1 Algorithmic Efficiency and Linear Memory Layout/putZeroEnd.py
@@ -0,0 +1,7 @@
+# The Problem:You are given an array of integers. Some of these integers are 0.Your goal is to move all the 0s to the very end of the array, while maintaining the exact relative order of all the non-zero elements.
+
+# Example of the desired outcome:If memory starts as: [0, 1, 0, 3, 12]Memory must end as: [1, 3, 12, 0, 0]
+
+# Strict Constraints:Space Complexity: Strictly $O(1)$. (Definition: You may use a few individual variables for pointers or temporary holds, but you cannot create a second array/list. You must mutate the existing array in-place).Operations: You must minimize the total number of operations.
+
+print("Hello")
